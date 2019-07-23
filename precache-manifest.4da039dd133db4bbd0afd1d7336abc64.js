6f90be20fd7c72c941d1b08bf0ce4d30dea5ba6c
CHELVIVIL96 <keylor139@gmail.com>
2019-07-23 15:37:05 -0600

    Updates

---
+++ precache-manifest.4da039dd133db4bbd0afd1d7336abc64.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6399216c11e932fe8d88222f81fa686",
+    "url": "/tutorial-react/index.html"
+  },
+  {
+    "revision": "b68aec382946f789aa96",
+    "url": "/tutorial-react/static/css/main.d11bb624.chunk.css"
+  },
+  {
+    "revision": "0bbef7593c1e3ae88319",
+    "url": "/tutorial-react/static/js/2.ef006c3a.chunk.js"
+  },
+  {
+    "revision": "b68aec382946f789aa96",
+    "url": "/tutorial-react/static/js/main.ebc6c97c.chunk.js"
+  },
+  {
+    "revision": "7b5cd42f9b7e4e484251",
+    "url": "/tutorial-react/static/js/runtime~main.a3145688.js"
+  }
+]);
